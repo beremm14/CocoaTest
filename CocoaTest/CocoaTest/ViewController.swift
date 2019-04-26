97b0a9b25e3ca43acf5cf89bf817e521b14d3255
Emil Berger <beremm14@htlkaindorf.at>
2019-04-26 15:37:17 +0200

    Init Project

---
+++ CocoaTest/CocoaTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaTest
+//
+//  Created by Emil Berger on 26.04.19.
+//  Copyright © 2019 Emil Berger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
